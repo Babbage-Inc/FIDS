f0c2e3cffbed0999a5dac9699d9169380d673e75
Bishal Pahari <bishal.pahari53@gmail.com>
2023-07-12 00:51:20 +0545

    refactor: Update mapping JSON directly to JSON destructuring

--- src/components/Table/TableLetter.jsx
+++ src/components/Table/TableLetter.jsx
@@ -1,17 +1,3 @@
-// import { useState } from "react";
-
-// const TableLetter = ({ letter, index }) => {
-//   const [flip, setFlip] = useState(false);
-
-//   setTimeout(() => {
-//     setFlip(true);
-//   }, 100 * index);
-
-//   return <div className={flip ? "flip" : null}>{flip ? letter : null}</div>;
-// };
-
-// export default TableLetter;
-
 import React, { useState, useEffect } from "react";
 
 const TableLetter = ({ letter, index }) => {
--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import TableCell from "./TableCell";
 
-const TableRow = ({ flight }) => {
+const TableRow = ({ flight, selectedOption }) => {
   const [previousFlight, setPreviousFlight] = useState(flight);
   const words = Object.values(flight);
+  console.log(words);
 
   useEffect(() => {
     // Compare the new 'flight' data with the previous data
@@ -15,13 +16,22 @@ const TableRow = ({ flight }) => {
 
   return (
     <tr>
-      {words?.map((word, index) => {
+      {/* {words?.map((word, index) => {
         // Conditionally render the TableCell component based on changes in 'word'
         if (word !== previousFlight[index]) {
-          return <TableCell key={index} word={word} />;
+          return (
+            <TableCell
+              key={index}
+              word={word}
+              flight={flight}
+              selectedOption={selectedOption}
+            />
+          );
         }
         return null;
-      })}
+      })} */}
+
+      <TableCell word={words} flight={flight} selectedOption={selectedOption} />
     </tr>
   );
 };
