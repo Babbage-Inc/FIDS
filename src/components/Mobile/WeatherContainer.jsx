c8a7361c90967fa93c6854a39f3f737e5116c187
Bishal Pahari <bishal.pahari53@gmail.com>
2023-07-13 14:12:06 +0545

    fix: Include API secret key in .env file

--- src/components/Mobile/WeatherContainer.jsx
+++ src/components/Mobile/WeatherContainer.jsx
@@ -9,8 +9,7 @@ import himalayan from "./assets/himalayan.png";
 
 const WeatherContainer = () => {
   const [data, setData] = useState({});
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=1a50eea33427e3b8e88c2a957dfc6bed&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=${process.env.REACT_APP_SECRET}&units=metric`;
 
   useEffect(() => {
     axios
